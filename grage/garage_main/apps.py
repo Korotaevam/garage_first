e20a346f539f9c12fb11d7e64936dbadb90476b5
Korotaevam <kam888@mail.ru>
2023-02-16 16:17:35 +0300

    init

---
+++ grage/garage_main/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GarageMainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'garage_main'
---
+++ grage/garage_main/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+from garage_main.views import *
+
+
+urlpatterns = [
+    path('', index),
+]
