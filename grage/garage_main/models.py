dbec1b379b5b23f13fbd51051a9596075d411daa
Korotaevam <kam888@mail.ru>
2023-02-17 14:56:03 +0300

    first shell

--- grage/garage_main/models.py
+++ grage/garage_main/models.py
@@ -11,7 +11,7 @@ class AutoModels(models.Model):
     brand = models.CharField(max_length=255, blank=True)
     auto_model = models.CharField(max_length=255, blank=True)
     quantity = models.IntegerField(default=0, validators=[MinValueValidator(0)])
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(default=0, max_digits=10, decimal_places=2)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     content_extra = models.TextField(blank=True)
@@ -19,3 +19,7 @@ class AutoModels(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.vendor
+
