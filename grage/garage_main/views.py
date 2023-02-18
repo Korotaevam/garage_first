4a35fdb8c846fadc1e389bee8a08cefc7d577eb0
Korotaevam <kam888@mail.ru>
2023-02-18 11:36:48 +0300

    load database

--- grage/garage_main/views.py
+++ grage/garage_main/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('<h1> Main Page </h1>')
 
+
 def category(request, cat_id=0):
-    if cat_id>10:
+    if cat_id > 10:
         return redirect('home')
     return HttpResponse(f'<h1> Cat Page <br> {cat_id}</h1>')
